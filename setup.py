911383f0870064c66163a4bdc5485674dde97c4d
Mykola Grymalyuk <khronokernel@icloud.com>
2025-04-30 13:05:32 -0600

    CI: Build and upload wheel

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+def fetch_property(property: str) -> str:
+    """
+    Fetch a property from simplemdm_sdk.
+    """
+    for line in open("simplemdm_sdk/__init__.py", "r").readlines():
+        if not line.startswith(property):
+            continue
+        return line.split("=")[1].strip().strip('"')
+    raise ValueError(f"Property {property} not found.")
+
+
+setup(
+    name='simplemdm_sdk',
+    version=fetch_property("__version__:"),
+    description=fetch_property("__description__:"),
+    long_description=open("README.md", "r").read(),
+    long_description_content_type='text/markdown',
+    author_email=fetch_property("__author_email__:"),
+    author=fetch_property("__author__:"),
+    license=fetch_property("__license__:"),
+    url=fetch_property("__url__:"),
+    python_requires='>=3.6',
+    packages=find_packages(include=["simplemdm_sdk"]),
+    package_data={
+        "simplemdm_sdk": ["*"],
+        "simplemdm_sdk._rest_api_endpoints": ["*"],
+    },
+    py_modules=["simplemdm_sdk"],
+    install_requires=open("requirements.txt", "r").readlines(),
+)
--- simplemdm_sdk/__init__.py
+++ simplemdm_sdk/__init__.py
@@ -1,6 +1,23 @@
+"""
+SimpleMDM SDK for Python
+
+Library usage:
+   >>> from simplemdm_sdk import SimpleMDM
+
+   >>> simplemdm_obj = SimpleMDM("your_api_key")
+   >>> devices = simplemdm_obj.devices.list_all()
+   >>> print(devices.data[0].attributes.name)
+"""
 
 from ._simplemdm import SimpleMDM
 
-__all__ = [
-    "SimpleMDM",
-]
+
+__title__:        str = "simplemdm_sdk"
+__version__:      str = "0.0.1"
+__description__:  str = "Highly opinionated SimpleMDM Python library"
+__url__:          str = "https://github.com/ripeda/SimpleMDM-SDK-Python"
+__author__:       str = "RIPEDA Consulting"
+__author_email__: str = "info@ripeda.com"
+__status__:       str = "Alpha"
+__license__:      str = "BSD 3-Clause License"
+__all__:         list = ["SimpleMDM"]
