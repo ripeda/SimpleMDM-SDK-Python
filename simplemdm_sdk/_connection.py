48cc99077edf649daedb63b89ea6fb3f1112ed16
Mykola Grymalyuk <khronokernel@icloud.com>
2025-05-08 14:50:52 -0600

    _connection.py: Use variable for URL

--- simplemdm_sdk/_connection.py
+++ simplemdm_sdk/_connection.py
@@ -73,7 +73,7 @@ class Connection:
         """
         function = getattr(self._session, method)
         while True:
-            if not (method == "patch" and url.startswith("https://a.simplemdm.com/api/v1/custom_configuration_profiles")):
+            if not (method == "patch" and url.startswith(f"{self._base}/custom_configuration_profiles")):
                 response: requests.Response = function(url, params=params, files=files)
             else:
                 # Specifying Content-Type results in a 500 error on 'https://a.simplemdm.com/api/v1/custom_configuration_profiles'
